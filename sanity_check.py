2649314acad87f3fa8b4332eafb35d3ef084a67f
it.zvision <it.zvision@yahoo.com>
2010-08-12 02:12:28 +0200

    add sanity_check.py
    Signed-off-by: it.zvision <it.zvision@yahoo.com>

---
+++ sanity_check.py
@@ -0,0 +1,91 @@
+
+from shutil import copy
+from shutil import rmtree
+
+import xmlrpclib
+import glob, os, sys, time
+
+'''
+This python module is meant to perform a sanity check for XML-RPC Service in KEA 5.0. 
+The check is performed according to following steps:
+- Given a set of .txt files have the stand alone KEA output .key files into provided folder, 
+- Start the XML-RPC service on localhost using provided port,
+- Have it generate corresponding set of key phrases and 
+- Print out both sets into console for compare
+
+'''
+
+#Read the port number 
+PORT=sys.argv[1]
+print "PORT= %s"%PORT
+
+KEA_HOME=os.environ.get("KEAHOME")
+if KEA_HOME == None:
+	os.system("setenv.bat")
+
+#Read test folder
+TEST_DIR=KEA_HOME+"\\"+sys.argv[2]
+print "TEST_DIR= ", TEST_DIR
+	 
+#Read the model 
+MY_MODEL = sys.argv[3]
+PATCH_LIB=KEA_HOME+"\\patch\\kea_server.jar"
+
+#adjust the xmlrpc library path
+XMLRPC_LIB="E:\\programs\\tools\\xmlrpc-1.2-b1\\xmlrpc-1.2-b1.jar"
+
+
+print "Delete existing .key files in %s prior KEA keyword extraction" %TEST_DIR
+if os.path.exists(TEST_DIR):
+	for keyFile in glob.glob( os.path.join(TEST_DIR, '*.key') ):
+		os.remove(keyFile)
+
+print "Running KEA keyword extraction..."
+JAVA=os.environ.get("JAVA_HOME")+"\\bin\\java.exe"
+os.system("%s kea.main.KEAKeyphraseExtractor -l %s -m %s -v none -n 10 "%(JAVA, TEST_DIR, MY_MODEL))
+
+print "Done keyword extraction, find generated .key files in %s" %TEST_DIR
+
+print "Starting server on port %s" %PORT
+TEST_CLASSPATH="%s;%s;%s"%(PATCH_LIB, XMLRPC_LIB, os.environ.get("CLASSPATH"))
+KEA_SERVER_START="%s -cp %s patch.main.KEAServer %s %s"%(JAVA, TEST_CLASSPATH, MY_MODEL, PORT)
+#print "Server start command %s" %KEA_SERVER_START
+
+print 
+#os.system(("start \'Running KEA XMLRPC Service\' cmd /C %s %s ")%(KEA_SERVER_START, PORT))
+
+#Start KEA XMLRPC Service
+os.system("start \"%s\" cmd /C %s "%('Running KEA XMLRPC Service', KEA_SERVER_START))
+
+print "\nWaiting 10 seconds for KEA XMLRPC Service to come up..."  
+a = 0
+while a < 10:
+	time.sleep(1)
+	a= a+1
+	print "."
+
+print "Done waiting"
+
+#Start the WebServer 
+
+KEA_SERVER = xmlrpclib.ServerProxy('http://127.0.0.1:'+PORT)
+
+for infile in glob.glob( os.path.join(TEST_DIR, '*.txt') ):
+    print "current file is: " + infile
+    print "================================================================\n"
+
+    txt = open(infile).read()
+    keywords = KEA_SERVER.kea.extractKeyphrases(txt)
+    for str in keywords:
+        print str
+        
+    #print contents of corresponding .key file
+    path, fi = os.path.split(infile)
+    name, ext = fi.rsplit('.')
+    keaKeyFile = path+'\\'+name+'.key'
+    print "\ncontents of corresponding .key file ", keaKeyFile ," from KEA."
+    print "........................................................................\n"
+    txt = open(keaKeyFile).read()
+    print txt
+    
+print "All Done."
--- sanity_check_Readme.txt
+++ sanity_check_Readme.txt
@@ -8,49 +8,18 @@ Perquisites:
 
 Steps:
 
--	create a folder in KEAHOME directory  (e.g.  KEAHOME\my_testing)
--	select a subset of test documents from KEAHOME\testdocs and copy to  KEAHOME\my_testing
--	run KEAKeyphraseExtractor with parameter settings below: 
-java kea.main.KEAKeyphraseExtractor -l KEAHOME\my_testing -m my_model  -v none -n 10
+-	create a folder in KEAHOME directory (e.g.  KEAHOME\my_testing)
+-	select and copy a subset of test documents from KEAHOME\testdocs to KEAHOME\my_testing
+- copy sanity_check.py into KEAHOME
+- perform the check by typing command below:
+			
+			python sanity_check.py <port> <folder> <model>
+		
+		e.g.
+		
+			python sanity_check.py 8090 my_testing mymodel
 
--	start the KEAServer: 
-java patch.main.KEAServer path\to\my_model 8090
-
--	start python and import  the sanity_check.py, see listing below. 
-
-
-
-sanity_check.py
-‘’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’
-import xmlrpclib
-import os, glob
-
-s = xmlrpclib.ServerProxy('http://127.0.0.1:8090')
-
-#adjust your KEAHOME path
-path = 'E:\\tmp\\kea-5.0_full\\my_testing\\'        
-for infile in glob.glob( os.path.join(path, '*.txt') ):
-    print "current file is: " + infile
-    print "================================================================\n"     
-    txt = open(infile).read()
-    keywords = s.kea.extractKeyphrases(txt)    
-    for str in keywords:
-        print str     
-        
-    #print contents of corresponding .key file
-    path, fi = os.path.split(infile) 
-    name, ext = fi.rsplit('.')
-    keaKeyFile = path+'\\'+name+'.key'
-    print "\ncontents of corresponding .key file ", keaKeyFile ,"from KEA."
-    print "........................................................................\n"
-    txt = open(keaKeyFile).read()
-    print txt
-    
-‘’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’’
-
-With two documents as input  
-
-You get the following output from sanity_chek.py
+With two documents as input you get the following output from sanity_chek.py
 
 current file is: E:\tmp\kea-5.0_full\my_testing\gtz_g24ine.txt
 ================================================================
